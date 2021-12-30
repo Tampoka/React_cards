0d9251b8e2d66249a6e43ff3135037c72e452d77
Tampoka <goal.thanks@gmail.com>
2021-12-30 15:14:24 +0900

    Add SuperSelect, SuperRadio.

--- src/pages/Test/TestingComponents.tsx
+++ src/pages/Test/TestingComponents.tsx
@@ -1,9 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 import SuperInputText from "../../common/components/c1-SuperInputText/SuperInputText";
 import SuperButton from "../../common/components/c2-SuperButton/SuperButton";
 import SuperCheckbox from "../../common/components/c3-SuperCheckbox/SuperCheckbox";
 import Loader from "../../common/components/Loader/Loader";
 import SuperEditableSpan from "../../common/components/c4-SuperEditableSpan/SuperEditableSpan";
+import SuperSelect from "../../common/components/c5-SuperSelect/SuperSelect";
+import SuperRadio from "../../common/components/c6-SuperRadio/SuperRadio";
 
 const TestingComponents = () => {
     return (
@@ -11,9 +13,10 @@ const TestingComponents = () => {
             <SuperInputText/>
             <SuperButton>Submit</SuperButton>
             <SuperCheckbox/>
+            <SuperEditableSpan value="enter text"/>
+            <SuperSelect options={['1', '2', '3']}   value={'1'}/>
+            <SuperRadio options={['1', '2', '3']}  name="radio" value={'1'} />
             <Loader/>
-            <SuperEditableSpan  value="enter text"
-            />
         </>
     );
 };
