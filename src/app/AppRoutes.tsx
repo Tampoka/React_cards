af7b39a7a65b4e942fa0a2c8283dd927b2909c67
Tampoka <goal.thanks@gmail.com>
2021-12-30 11:50:29 +0900

    Add all routes + links.

--- src/app/AppRoutes.tsx
+++ src/app/AppRoutes.tsx
@@ -1,9 +1,12 @@
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import TestingComponents from "../pages/Test/TestingComponents";
 import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
 import Profile from "../pages/Profile/Profile";
 import Error404 from "../pages/Error404/Error404";
+import s from "./AppRoutes.module.scss"
+import RestorePassword from "../pages/RestorePassword/RestorePassword";
+import NewPassword from "../pages/NewPassword/NewPassword";
 
 export const Path = {
     home: '/',
@@ -11,18 +14,26 @@ export const Path = {
     profile: 'profile',
     login: 'login',
     register: 'register',
+    error: '404',
+    restorePassword: 'restore-password',
+    newPassword: 'new-password',
 }
 
 function AppRoutes() {
     return (
-        <Routes>
-            <Route path={Path.home} element={<TestingComponents/>}/>
-            <Route path={Path.test} element={<TestingComponents/>}/>
-            <Route path={Path.profile} element={<Profile/>}/>
-            <Route path={Path.login} element={<Login/>}/>
-            <Route path={Path.register} element={<Register/>}/>
-            <Route path="*" element={<Error404/>}/>
-        </Routes>
+        <div className={s.content}>
+            <Routes>
+                <Route path={Path.home} element={<TestingComponents/>}/>
+                <Route path={Path.test} element={<TestingComponents/>}/>
+                <Route path={Path.profile} element={<Profile/>}/>
+                <Route path={Path.login} element={<Login/>}/>
+                <Route path={Path.register} element={<Register/>}/>
+                <Route path={Path.error} element={<Error404/>}/>
+                <Route path={Path.restorePassword} element={<RestorePassword/>}/>
+                <Route path={Path.newPassword} element={<NewPassword/>}/>
+                <Route path="*" element={<Navigate to="/404"/>}/>
+            </Routes>
+        </div>
     )
 }
 
