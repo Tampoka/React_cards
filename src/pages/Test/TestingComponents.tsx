97b22ab6e1699324c92c1117ac107f35c60a767f
Tampoka <goal.thanks@gmail.com>
2021-12-30 13:00:23 +0900

    Add SuperInputText.

--- src/pages/Test/TestingComponents.tsx
+++ src/pages/Test/TestingComponents.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
+import SuperInputText from "../../common/components/c1-SuperInputText/SuperInputText";
+import SuperButton from "../../common/components/c2-SuperButton/SuperButton";
+import SuperCheckbox from "../../common/components/c3-SuperCheckbox/SuperCheckbox";
 
 const TestingComponents = () => {
     return (
-        <div>
-            Test
-        </div>
+        <>
+            <SuperInputText/>
+            <SuperButton/>
+            {/*<SuperCheckbox/>*/}
+        </>
     );
 };
 
