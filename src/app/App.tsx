3d4712085080583ef8ea0b40cfc2e8f88402da5f
Tampoka <goal.thanks@gmail.com>
2021-12-30 11:50:44 +0900

    Finish header.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -13,7 +13,6 @@ function App() {
         <div>
             <Header/>
             <AppRoutes/>
-
         </div>
     );
 }
--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from "react";
 import {NavLink} from "react-router-dom";
 import {Path} from "../../../app/AppRoutes";
+import s from "./Header.module.scss"
 
 const Header = () => {
     return (
-        <div>
-            <div>Menu</div>
-            <div>
+        <div className={s.header}>
+            <div className={s.navLinks}>
                 <NavLink to={Path.home}>Home</NavLink>
-                <NavLink to={Path.profile}
-                >Profile</NavLink>
+                <NavLink to={Path.profile}>Profile</NavLink>
                 <NavLink to={Path.login}>Login</NavLink>
-                <NavLink to={Path.test}>Test</NavLink>
-                <NavLink to={Path.register}
-                >Register</NavLink>
+                <NavLink to={Path.register}>Register</NavLink>
+                <NavLink to={Path.error}>Error404</NavLink>
+                <NavLink to={Path.newPassword}>New Password</NavLink>
+                <NavLink to={Path.restorePassword}>Restore Password</NavLink>
             </div>
         </div>
     );
