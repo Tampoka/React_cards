c5582e5dbd8d603878f6caba6b4349d9150964aa
Tampoka <goal.thanks@gmail.com>
2022-01-10 23:41:42 +0900

    Change error text.

--- src/pages/Error404/Error404.tsx
+++ src/pages/Error404/Error404.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Error404 = () => {
     return (
-        <div>
-            Error 404
-        </div>
+        <h1>
+            Page not found. Error: 404
+        </h1>
     );
 };
 
