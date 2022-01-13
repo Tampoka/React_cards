4f30ea33b8efdd44f368cc9d371e8a18d0dee1aa
Tampoka <goal.thanks@gmail.com>
2022-01-13 14:31:45 +0900

    Add appInfo.

--- src/redux/app-reducer.ts
+++ src/redux/app-reducer.ts
@@ -5,6 +5,7 @@ const initialState = {
     error: null as null | string,
     //true if app already/successfully initialized (user authentication, settings etc.)
     isInitialized: false,
+    appInfo: '',
 }
 
 export const appReducer = (state: InitialStateType = initialState, action: AppActionsType): InitialStateType => {
@@ -15,6 +16,8 @@ export const appReducer = (state: InitialStateType = initialState, action: AppAc
             return {...state, error: action.payload.error}
         case 'APP/SET-IS-INITIALIZED':
             return {...state, isInitialized: action.payload.value}
+        case 'APP/SET-APP-INFO':
+            return {...state, appInfo: action.payload.message}
         default:
             return {...state}
     }
@@ -24,6 +27,7 @@ export const appReducer = (state: InitialStateType = initialState, action: AppAc
 export const setAppError = (error: string | null) => ({type: 'APP/SET-ERROR', payload: {error}} as const)
 export const setAppIsLoading = (isLoading: boolean) => ({type: 'APP/SET-IS-LOADING', payload: {isLoading}} as const)
 export const setAppInitialized = (value: boolean) => ({type: 'APP/SET-IS-INITIALIZED', payload: {value}} as const)
+export const setAppInfo = (message: string) => ({type: 'APP/SET-APP-INFO', payload: {message}} as const)
 
 //Thunk Creators
 export const initializeApp = (): ThunkType => async dispatch => {
@@ -43,9 +47,11 @@ export type InitialStateType = typeof initialState
 export type SetAppErrorActionType = ReturnType<typeof setAppError>
 export type SetAppIsLoadingActionType = ReturnType<typeof setAppIsLoading>
 export type SetAppInitializedActionType = ReturnType<typeof setAppInitialized>
+export type SetAppInfoActionType = ReturnType<typeof setAppInfo>
 
 export type AppActionsType =
     | SetAppInitializedActionType
     | SetAppIsLoadingActionType
     | SetAppErrorActionType
+    | SetAppInfoActionType
 
