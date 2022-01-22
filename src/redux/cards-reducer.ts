fd91ceac03596513cc01618895b16351652cc9ce
Tampoka <goal.thanks@gmail.com>
2022-01-22 12:29:58 +0900

    Add initial state.

--- src/redux/cards-reducer.ts
+++ src/redux/cards-reducer.ts
@@ -1,3 +1,37 @@
-export {
+import {CardsResponse} from '../api/cards-api';
 
+const initialState:CardsInitialStateType={
+    cards:[],
+    page: 1,
+    pageCount: 10,
+    cardsTotalCount: 0,
+    packUserId: '',
+    minGrade: 0,
+    maxGrade: 0,
+    currentCardsPackID: '',
+    sortCardsMethod: undefined,
+    currentGrade: [0, 0],
+    countPerPage: [10, 25, 50]
 }
+
+// export const cardsReducer=(state=initialState,action:CardsActionsType):CardsInitialStateType=>{
+//     switch (action.type) {
+//         default:
+//             return state
+//     }
+// }
+//
+
+
+
+
+
+//TYPES
+
+export type CardsInitialStateType = CardsResponse & {
+    currentCardsPackID: string
+    sortCardsMethod: string | undefined
+    currentGrade: number[]
+    countPerPage: number[]
+}
+// export type CardsActionsType={}
