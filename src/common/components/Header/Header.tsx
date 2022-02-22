1786f61ec1b0ed527ff1bf82480388d2a769cd6e
Tampoka <goal.thanks@gmail.com>
2022-02-22 20:16:47 +0900

    Add login/logout button.

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {NavLink} from 'react-router-dom';
+import {NavLink, useNavigate} from 'react-router-dom';
 import s from './Header.module.scss'
 import SuperButton from '../SuperButton/SuperButton';
 import {useDispatch} from 'react-redux';
@@ -9,6 +9,12 @@ import {logOut} from '../../../redux/auth-reducer';
 type NavListType = Array<{ title: string, to: string }>;
 
 const Header = React.memo(() => {
+        const dispatch = useDispatch()
+
+        const navigate = useNavigate()
+
+        const isLoggedIn = useAppSelector<boolean>(state => state.auth.isLoggedIn)
+
         const [showDevPages, setShowDevPages] = useState(false)
         const navList: NavListType = [
             {title: 'Home', to: '/'},
@@ -23,14 +29,18 @@ const Header = React.memo(() => {
             {title: 'Check Email', to: '/check-email'},
         ]
         const mappedNavList = navList.map(({to, title}) => <NavLink to={to}
-                                                                    className={({isActive}) => (isActive ? s.active : '')}
+                                                                    className={({isActive}) => (isActive
+                                                                        ? s.active
+                                                                        : '')}
                                                                     key={to}>{title}</NavLink>)
         const mappedDevPages = devPages.map(({to, title}) => <NavLink to={to}
-                                                                      className={({isActive}) => (isActive ? s.active : '')}
+                                                                      className={({isActive}) => (isActive
+                                                                          ? s.active
+                                                                          : '')}
                                                                       key={to}>{title}</NavLink>)
-        const dispatch = useDispatch()
+
         const signOut = () => dispatch(logOut())
-        const isLoggedIn = useAppSelector<boolean>(state => state.auth.isLoggedIn)
+
         return (
             <div className={s.header}>
                 <div className={s.navLinks}>
@@ -38,10 +48,16 @@ const Header = React.memo(() => {
                 </div>
                 <div className={s.devPages}>
                     <SuperButton
-                        onClick={() => setShowDevPages(!showDevPages)}>{!showDevPages ? 'Show dev pages' : 'Hide dev pages'}</SuperButton>
+                        onClick={() => setShowDevPages(!showDevPages)}>{!showDevPages
+                        ? 'Show dev pages'
+                        : 'Hide dev pages'}</SuperButton>
                     {showDevPages && mappedDevPages}
                 </div>
-                {isLoggedIn && <SuperButton onClick={signOut}>Log out</SuperButton>}
+                {isLoggedIn
+                    ? <SuperButton
+                        onClick={signOut}>Log out</SuperButton>
+                    : <SuperButton
+                        onClick={()=>navigate('/login')}>Log in</SuperButton>}
             </div>
         )
     }
