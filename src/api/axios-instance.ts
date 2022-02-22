2f1deb924b9f447ada45978e07a1bc5114116e25
Tampoka <goal.thanks@gmail.com>
2022-02-22 20:02:16 +0900

    Add environment variables.

--- src/api/axios-instance.ts
+++ src/api/axios-instance.ts
@@ -1,17 +1,12 @@
 import axios from 'axios';
 
-/*
 export const developmentMode = true
 
 export const instance = axios.create({
-    baseURL: developmentMode ? process.env.REACT_APP_DEVELOPMENT_MODE_BASE_URL : process.env.REACT_APP_BASE_URL,
+    baseURL: developmentMode
+        ? process.env.REACT_APP_DEVELOPMENT_MODE_BASE_URL
+        : process.env.REACT_APP_BASE_URL,
     withCredentials: true,
 })
-*/
 
 
-export const instance = axios.create({
-    baseURL: 'http://localhost:7542/2.0/',
-    // baseURL: 'https://neko-back.herokuapp.com/2.0',
-    withCredentials: true,
-});
