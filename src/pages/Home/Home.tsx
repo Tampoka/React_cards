335e193a373d48b57b5ecceacde7ee5344199653
Tampoka <goal.thanks@gmail.com>
2022-02-22 12:16:43 +0900

    Add test account.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,11 +4,23 @@ import {Loader} from '../../common/components/Loader/Loader';
 import {useDispatch} from 'react-redux';
 import {useAppSelector} from '../../redux/store';
 import s from './Home.module.scss'
+import {NavLink} from 'react-router-dom';
+import {ROUTES} from '../../enums/routes/routes';
+import SuperButton from '../../common/components/SuperButton/SuperButton';
+import {login} from '../../redux/auth-reducer';
 
 export const Home = () => {
     const dispatch = useDispatch()
 
     const isInitialized = useAppSelector(state => state.app.isInitialized)
+
+    const setTestAccountLogin = () => {
+        const email = process.env.REACT_APP_TEST_EMAIL
+        const password = process.env.REACT_APP_TEST_PASSWORD
+        console.log(email,password)
+        if (!email || !password) return
+        dispatch(login({email, password, rememberMe: true}))
+    }
 
     useEffect(() => {
         dispatch(initializeApp())
@@ -18,7 +30,21 @@ export const Home = () => {
 
     return (
         <div className={s.homeContainer}>
-          <h1>Welcome to Learning Cards</h1>
+            <h1>Welcome to Learning Cards</h1>
+            <div className={s.description}>
+                <p>Studying language, preparing for exam <br/>
+                    or just have to memorize some <span>Javascript</span> concepts?</p>
+                <p>Using our Learning Cards app you can learn <strong>anytime, anywhere</strong>.</p>
+            </div>
+            <div className={s.instructions}>
+                <p>Choose <span>Decks</span> ⟶ <span>Learn</span> and start answering questions or create your
+                    own <span>Cards</span></p>
+                <p className={s.rate}>✔ You can rate your answers so that answers with lower rate will display more
+                    often to memorize things better.</p>
+                <p className={s.test}>Before <NavLink to={ROUTES.REG}>register</NavLink>, you can discover features
+                    with <SuperButton onClick={setTestAccountLogin}>test account</SuperButton>.</p>
+            </div>
+
         </div>
     );
 };
